c9ad53d4b12cdef8e7e13f54e3b790f6b785c059
AndreesaARodrigues <andressaalmeida.aluno@unipampa.edu.br>
2023-06-09 15:05:04 -0300

    Handling Form Submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -40,8 +40,21 @@ const ExepenseForm = () => {
         });*/
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-    return <form>
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+    };
+
+
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
